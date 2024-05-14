7427ef81e2506262d1ca875250d7664534e8f4f1
Eda S <166115497+ShnEda@users.noreply.github.com>
2024-05-14 13:18:44 +0300

    gecis

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
-#import <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    printf("hello");
+int menu() {
+    int secim;
+    printf("\n\t     **MUSTERI EKRANI**\n\n");
+    printf("\t 1 - YENI SIPARIS \n");
+    printf("\t 2 - MEVCUT SIPARIS DURUMLARI \n");
+    printf("\t 3 - DAHA ONCEKI SIPARISLERIM \n");
+    printf("\t 0 - PROGRAMI KAPAT \n\n");
+    printf("\t Seciminiz     : ");
+    scanf("%d",&secim);
+    system("cls");
+    return secim;
+}
+
+int main() {
+
+    int secim = menu();
+    while(secim!=0) {
+        switch(secim) {
+
+            case 1: yeniSiparis(); break;
+            case 2: mevcutSiparis(); break;
+            case 3: oncekiSiparis(); break;
+            case 0: break;
+            default: printf("hatali secim yaptiniz ! \n"); break;
+        }
+        secim = menu();
+    }
+
+
     return 0;
 }
