ca0919a985a874ddaddf550e62dde70ede4a425b
TurkanAYT <168912407+TurkanAYT@users.noreply.github.com>
2024-05-19 23:37:41 +0300

    Update and rename mmain.c to Musteri_main.c
    kullanıcı olayı eklendi

---
+++ proje/Musteri_main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include "musteri.h"
+
+int main() {
+
+    int secim = musteriUygulamasi();
+    while (secim!=0) {
+        switch (secim) {
+            case 1: kayitOl(); break;
+            case 2:
+                if (oturumAc()) {
+                    int secim2 = musteriEkrani();
+                    while(secim2!=0) {
+                        switch(secim2) {
+                            case 1: yeniSiparis(); break;
+                            case 2: mevcutSiparis(); break;
+                            case 3: oncekiSiparis(); break;
+                            case 0: return musteriUygulamasi(); break;
+                            default: printf("hatali secim yaptiniz ! \n"); break;
+                        }
+                        secim2 = musteriEkrani();
+                    }
+                }
+                else {
+                    printf("kullanici adi veya sifre hatali!!\n");
+                    printf("\nAna menuye donmek icin bir tusa basiniz..");
+                    getch();
+                    system("cls");
+                }
+
+                break;
+            case 0: return 0;
+            default: printf("hatali secim yaptiniz ! \n"); break;
+        }
+        secim = musteriUygulamasi();
+    }
+
+    return 0;
+}
