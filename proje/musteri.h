1c897b604721124290e3f3d0a81d310db4b64443
TurkanAYT <168912407+TurkanAYT@users.noreply.github.com>
2024-05-24 15:52:37 +0300

    Update musteri.h

--- proje/musteri.h
+++ proje/musteri.h
@@ -1,31 +1,28 @@
 #ifndef _MUSTERI_H
 #define _MUSTERI_H
 
-#define MAX_UZUNLUK 30
-
 typedef struct Musteri {
     int ID, fiyat, hazirlama_suresi,yemekID, yemekBuldu;
-    char yemekAdi[MAX_UZUNLUK], durum[MAX_UZUNLUK], tarih[MAX_UZUNLUK],
-         saat[MAX_UZUNLUK], satir[100], kullaniciAdi[MAX_UZUNLUK], sifre[MAX_UZUNLUK];
+    char yemekAdi[30], durum[15], tarih[30],
+         saat[30], satir[100], kullaniciAdi[15],
+         sifre[8], sipID[10],sipZamani[30],hazZamani[30];
     time_t siparisTarihi, hazirOlmaZamani;
 } musteri ;
+
 
 
 int MusteriUygulamasi();
 int musteriEkrani();
 
-
 void kayitOl();
 int oturumAc();
-
 
 void yemekleriListele();
 void siparisAl();
 void yeniSiparis();
 
-int mevcutSiparis();
+void mevcutSiparis();
 
 void oncekiSiparis();
 
 #endif
-
