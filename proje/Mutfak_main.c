2a4f77eb46434ab53a5a8b6c97e3eacd5fe0d514
KtuEdaShn <434336@ogr.ktu.edu.tr>
2024-05-24 05:49:53 +0300

    kullanici alakalilarda duzenleme gerek

--- proje/Mutfak_main.c
+++ proje/Mutfak_main.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <Mutfak.h>
+#include <conio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+#include "Mutfak.h"
+
+/*
+ *
+ * void onaylı yemekleri Listele
+ * void onaylı yemeklerin suresini ilerlet vb
+ * void asci bitirince yemegi siparis guncelleme
+ *
+ * burada istenen neredeyse sadece yemek hazirlanma zamaninin
+ * guncellenmesi
+ * !!!Asci sadece 1 yemek yapabilir, en az aşçı sayisiyla
+ * (mumkunse mutfakta toplam asci sayisina gore yazilmali)
+
+*/
