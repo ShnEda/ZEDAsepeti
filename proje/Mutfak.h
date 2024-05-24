73bfde6f48446605c7b74018fdd37ac7bf56b516
KtuEdaShn <434336@ogr.ktu.edu.tr>
2024-05-24 05:30:50 +0300

    kullanici alakalilarda duzenleme gerek

--- proje/Mutfak.h
+++ proje/Mutfak.h
@@ -1,8 +1,19 @@
-//
-// Created by eda on 24.05.2024.
-//
-
 #ifndef ZEDASEPETI_MUTFAK_H
 #define ZEDASEPETI_MUTFAK_H
+
+/* typedef struct mutfak
+ *
+ * void onaylı yemekleri Listele
+ * void onaylı yemeklerin suresini ilerlet vb
+ * void asci bitirince yemegi siparis guncelleme
+ *
+ * burada istenen neredeyse sadece yemek hazirlanma zamaninin
+ * guncellenmesi
+ * !!!Asci sadece 1 yemek yapabilir, en az aşçı sayisiyla
+ * (mumkunse mutfakta toplam asci sayisina gore yazilmali)
+
+*/
+
+
 
 #endif //ZEDASEPETI_MUTFAK_H
