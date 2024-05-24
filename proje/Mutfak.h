a5e9272a1ae9229b1f62f46bb9839118bbebe4fc
KtuEdaShn <434336@ogr.ktu.edu.tr>
2024-05-24 06:09:56 +0300

    kullanici alakalilarda duzenleme gerek

--- proje/Mutfak.h
+++ proje/Mutfak.h
@@ -11,6 +11,17 @@
  * guncellenmesi
  * !!!Asci sadece 1 yemek yapabilir, en az aşçı sayisiyla
  * (mumkunse mutfakta toplam asci sayisina gore yazilmali)
+ *
+ * Bura da gpt yolu:
+ *- Onaylı Yemekleri Listeleme:(gerek var mi)
+ *- Baska bir uygulama veya sistemden alinan onayli yemek listesini oku
+mutfaga bu listedeki yemekleri listele
+* -Yemek Hazırlama Suresini İlerletme:
+Asci sectigi bir yemegin hazirlik suresini belirli araliklarla ilerlet.
+Bunu belirli bir yemegin hazirlik suresini artirmak icin kullan.
+* -Ascinin Yemegi Hazirlama ve Guncelleme:
+* -Asci bir yemegi hazirlamaya basladiginda, secilen yemegin hazirlik durumunu guncelle.
+Guncellenmis yemek listesini yeni bir dosyaya kaydet veya mevcut dosyayi guncelle.
 
 */
 
