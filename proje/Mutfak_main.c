f808609cf94f9a58e818f2373dc7a2a329c25098
KtuEdaShn <434336@ogr.ktu.edu.tr>
2024-05-26 09:15:15 +0300

    mutfak main guncelleme

--- proje/Mutfak_main.c
+++ proje/Mutfak_main.c
@@ -87,17 +87,3 @@ void atanmisYemekListele()
 
 
 }
-
-
-/*
- *
- * void onaylı yemekleri Listele
- * void onaylı yemeklerin suresini ilerlet vb
- * void asci bitirince yemegi siparis guncelleme
- *
- * burada istenen neredeyse sadece yemek hazirlanma zamaninin
- * guncellenmesi
- * !!!Asci sadece 1 yemek yapabilir, en az aşçı sayisiyla
- * (mumkunse mutfakta toplam asci sayisina gore yazilmali)
-
-*/
