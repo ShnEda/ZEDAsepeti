fb1fbf863a88d4091b806f80974dd99c129c9fd1
KtuEdaShn <434336@ogr.ktu.edu.tr>
2024-05-26 19:17:57 +0300

    MUS 3 STRUCT BB

--- proje/musteri.h
+++ proje/musteri.h
@@ -8,8 +8,8 @@
 
 typedef struct Musteri {
     int ID, fiyat, hazirlama_suresi,yemekID, yemekBuldu;
-    char yemekAdi[30], durum[15], tarih[30],
-         saat[30], satir[100], kullaniciAdi[15],
+    char yemekAdi[30], durum[15],
+         kullaniciAdi[15],
          sifre[8], sipID[10],sipZamani[30],hazZamani[30];
     time_t siparisTarihi, hazirOlmaZamani;
 } musteri ;
